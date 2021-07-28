5a92fb1c2b5aeb8e36526c13b9f988f7be9e29b7
Dima <strebkov003@bk.ru>
2021-07-28 17:17:22 +0300

    Practick for js

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log(2 + 2 * 2 !== +'6');
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movie: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Один из просмотренных фильмов");
+const b = prompt("На сколько оцените его?");
+const c = prompt("Один из просмотренных фильмов");
+const d = prompt("На сколько оцените его?");
+
+personalMovieDB.movie[a] = b;
+personalMovieDB.movie[c] = d;
+console.log(personalMovieDB);
+ 
